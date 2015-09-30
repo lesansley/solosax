b8570100313724a59cc0cc5df1d0dbef5867d89a
Les Ansley <les@ansleyfamily.com>
2015-09-30 17:00:19 +0100

    Complete landing page functionality

--- js/javascript.js
+++ js/javascript.js
@@ -17,60 +17,69 @@ $.fn.moveIt = function() {
   }
 }
 
-function setBannerHeight() {
+// Make the height of the title-wrapper equal to the banner-img in order to vertically position text in the middle
+function setTitleWrapperHeight() {
   var bannerHeight = $('.banner-img').height();
   if(bannerHeight>0) {
     $('.title-wrapper').height(bannerHeight);
+    setTitleTextY(bannerHeight);
   }
 }
 
+function setTitleTextY(wrapperHeight) {
+  var navHeight = $('.top-nav').height();
+  var $titleText = $('.title');
+  var titleHeight = $titleText.height();
+  var titleTop = $titleText[0].offsetTop;
+  var move = (0.5 * (wrapperHeight - navHeight) - (0.5 * titleHeight) + navHeight) - titleTop;
+    $titleText.css('transform', 'translateY(' + move + 'px)')
+}
+
 function setNavBar(nav, logo) {
+  var $main = $('main');
   if (window.matchMedia('(max-width: 768px)').matches) {
-    var $main = $('main');
     nav.css('background-color', 'rgb(236,170,57)');
     nav.css('box-shadow', '0px 2px 5px black');
     nav.css('color', 'white');
     logo.css('visibility', 'visible');
     $main.css('padding-top', '100px');
+  } else{
+    $main.css('padding-top', '');
   }
 }
 
 function setNavStyle() {
-  var scroll = $( window ).scrollTop();
   var $nav = $('.top-nav');
   var $logo = $('.logo-img')
-
-  if (!window.matchMedia('(max-width: 768px)').matches) {
-    if(scroll != 0) {
-        $nav.css('background-color', 'rgb(236,170,57)');
-        $nav.css('box-shadow', '0px 2px 5px black');
-        $nav.css('color', 'white');
-        $logo.css('visibility', 'visible');
-    }
-    else {
-        $nav.css('background-color', '');
-        $nav.css('box-shadow', '');
-        $nav.css('color', 'rgb(236,170,57)');
-        $logo.css('visibility', 'hidden');
-    }
+  var scroll = $( window ).scrollTop();
+  if(scroll != 0) {
+      $nav.css('background-color', 'rgb(236,170,57)');
+      $nav.css('box-shadow', '0px 2px 5px black');
+      $nav.css('color', 'white');
+      $logo.css('visibility', 'visible');
   }
   else {
-    console.log('Call setNavBar');
-    setNavBar($nav, $logo);
+      $nav.css('background-color', '');
+      $nav.css('box-shadow', '');
+      $nav.css('color', 'rgb(236,170,57)');
+      $logo.css('visibility', 'hidden');
   }
+  setNavBar($nav, $logo);
 }
 
 $(function() {
   $('[data-scroll-speed]').moveIt();
+
   $( window ).resize(function() {
-    setBannerHeight();
     setNavStyle();
+    setTitleWrapperHeight();
   });
+
   $( window ).on('scroll', function() {
     setNavStyle();
   })
 });
 
-$( document).ready(function() {
-  console.log('loaded');
-})
+$('.banner-img').load(function() {
+  setTitleWrapperHeight();
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 Parallex code: http://codepen.io/JTParrett/pen/BkDie
+Quotes: https://css-tricks.com/snippets/css/simple-and-nice-blockquote-styling/
